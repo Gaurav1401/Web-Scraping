1c3a102f2b8e28960b8ecad21196ca8b0dfb4b9e
Gaurav1401 <gk.bhagat14@gmail.com>
2021-07-14 21:03:20 +0530

    extracting multiple elements

--- practice.py
+++ practice.py
@@ -10,12 +10,24 @@ soup = BeautifulSoup(website.text, 'html.parser')
 
 # Let's first try to get the title of the website
 title = soup.find('title')
-print(title.text)
+# print(title.text)
 
 # Extracting element based on tag
 link = soup.find('a') # will return the first <a> tag
-print(link)
+# print(link)
 
 # Extracting element based on class name
 quote = soup.find(class_ = 'text') # will return the first tag having the class 'text'
-print(quote.text) 
+# print(quote.text) 
+
+# Extracting many elements at the same time
+# Let's try to extract all the text of links available on website
+
+links = soup.find_all('a') # all the a tags
+#print(links) # It will return the whole tag in list format
+
+for i in links:
+    print(i.text) # Will retun all the texts which contains hyperlinks
+
+
+
