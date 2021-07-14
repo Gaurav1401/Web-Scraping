8a60902c97230f63a1d68bd37ee22c02155fbc72
Gaurav1401 <gk.bhagat14@gmail.com>
2021-07-14 20:40:50 +0530

    Extract title

--- practice.py
+++ practice.py
@@ -1,2 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+
+website = requests.get('https://quotes.toscrape.com/')
+# print(website) # It returns a response object
+# print(website.text) # It will return the whole HTML page of the website
+
+# Declaring BeautifulSoup Object
+soup = BeautifulSoup(website.text, 'html.parser')
+
+# Let's first try to get the title of the website
+title = soup.find('title')
+print(title.text)
