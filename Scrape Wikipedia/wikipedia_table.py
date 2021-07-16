f274424db8f7a0298118667960ec9c6194aff1d7
Gaurav1401 <gk.bhagat14@gmail.com>
2021-07-16 12:11:07 +0530

    country names

--- Scrape Wikipedia/wikipedia_table.py
+++ Scrape Wikipedia/wikipedia_table.py
@@ -4,5 +4,12 @@ import csv
 
 website = requests.get('https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population')
 
-soup = BeautifulSoup(website, 'html.parser')
+soup = BeautifulSoup(website.text, 'html.parser')
 
+first_table = soup.select_one('.wikitable')
+table_rows = first_table.select('tr')[2:]
+
+for row in table_rows[:3]:
+    table_data = row.select('td')
+    country = table_data[0].find('a').text
+    print(country)
