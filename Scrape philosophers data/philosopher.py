e33b716aab0f58c3dc8d1dcc1d70d6b37ec5956c
Gaurav1401 <gk.bhagat14@gmail.com>
2021-07-17 12:12:44 +0530

    Initial commit

---
+++ Scrape philosophers data/philosopher.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+# We are scrapping the data of first philosopher on:
+# https://www.britannica.com/topic/list-of-philosophers-2027173#ref327483
+# then we will scrape the data of rest
+website = requests.get('https://www.britannica.com/biography/Mortimer-J-Adler')
+
+soup = BeautifulSoup(website, 'html.parser')
+
+name = soup.select_one('h1')
+print(name)
